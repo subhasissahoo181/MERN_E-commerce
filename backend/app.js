74fa582006f9f25b1b46a48bdec485200b3f9f60
Subhasis <subhasissahoo181@gmail.com>
2024-01-09 08:56:16 +0530

    Connection db

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://eshop-tutorial-pyri.vercel.app',],
+  origin: ['https://mern-e-commerce-jade.vercel.app/',],
   credentials: true
 }));
 
