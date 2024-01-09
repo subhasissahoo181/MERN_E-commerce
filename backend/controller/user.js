f1481762dff24daebe22eab71de455beb4c4d1d3
Subhasis <subhasissahoo181@gmail.com>
2024-01-09 09:14:26 +0530

    Some api connection

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -35,7 +35,7 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `https://eshop-tutorial-pyri.vercel.app/activation/${activationToken}`;
+    const activationUrl = `https://mern-e-commerce-jade.vercel.app/activation/${activationToken}`;
 
     try {
       await sendMail({
