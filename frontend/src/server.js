f378b8fb9bcd3226ec9bd505531955e5ecc99bdf
Subhasis <subhasissahoo181@gmail.com>
2024-01-09 11:37:28 +0530

    server connection

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "https://mern-e-commerce-kappa.vercel.app/test/api/v2";
+export const server = "https://mern-e-commerce-kappa.vercel.app/api/v2";
 // export const server = "https://mern-e-commerce-kappa.vercel.app/api/v2";
 
 
