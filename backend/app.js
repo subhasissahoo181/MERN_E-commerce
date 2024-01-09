f2c3395cc3da86de758c717a677807ec092be263
Subhasis <subhasissahoo181@gmail.com>
2024-01-09 10:24:00 +0530

    Some api connect

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,16 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors({
-  origin: ['https://mern-e-commerce-jade.vercel.app/',],
+  origin: 'https://mern-e-commerce-atpm.vercel.app/',
   credentials: true
 }));
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/", express.static(path.join(__dirname,"./uploads")));
 app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
