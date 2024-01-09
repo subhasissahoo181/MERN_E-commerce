b58bd9c4a7b651731a02bf5dc1caceafb7743c3e
Subhasis <subhasissahoo181@gmail.com>
2024-01-09 10:53:33 +0530

    Socket add.

--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/styles";
-const ENDPOINT = "https://mern-e-commerce-sa53.onrender.com/";
+const ENDPOINT = "https://socket-ecommerce-o71x.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
